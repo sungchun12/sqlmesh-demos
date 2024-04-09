3bef29c416810a81f3610b5cdef8044c5eeef52b
Sung Won Chung <sungwonchung3@gmail.com>
2024-04-09 11:11:28 -0700

    make a non-breaking change

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -13,5 +13,6 @@ SELECT
   id AS payment_id,
   order_id,
   payment_method,
-  amount / 100 AS amount /* `amount` is currently stored in cents, so we convert it to dollars */
+  amount / 100 AS amount, /* `amount` is currently stored in cents, so we convert it to dollars */
+  "new_column" as new_column -- non-breaking change example
 FROM demo.seed_raw_payments
